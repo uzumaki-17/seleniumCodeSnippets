73692c946110653fdc904279db20c2e6565d78f6
uzumaki-17 <ujjwalgupta462@gmail.com>
2024-07-22 17:20:56 +0530

    code snippets selenium

---
+++ src/test/java/sessionSelenium/popupJavaScript.java
@@ -0,0 +1,39 @@
+package sessionSelenium;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class popupJavaScript {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		
+		String xpath = "//input[@name='proceed']";
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(xpath)).click();
+		
+		Alert alert = driver.switchTo().alert();
+		
+		String alertText = alert.getText().toString();
+		
+		if(alertText.equals("Please enter a valid user name"))
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+		
+		alert.accept();
+		Thread.sleep(3000);
+		driver.quit();
+		  
+
+	}
+
+}
---
+++ src/test/java/sessionSelenium/windowBasedPopup.java
@@ -0,0 +1,29 @@
+package sessionSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class windowBasedPopup {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String url = "https://the-internet.herokuapp.com/upload";
+		driver.get(url);
+		driver.findElement(By.xpath("//input[@id='file-upload']")).sendKeys("C:\\Users\\jyots\\eclipse-workspace\\sessionSelenium\\src\\test\\resources\\files\\ujjwal_pic.jpg");
+		driver.findElement(By.xpath("//input[@class='button']")).click();
+		String validate = "//div[@id='uploaded-files']";
+		if(driver.findElement(By.xpath(validate)).getText().equals("ujjwal_pic.jpg"))
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+		driver.quit();
+
+	}
+
+}
